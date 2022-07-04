9843349bb479f4b91864cb58ab50bad7370253c3
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-04 00:02:36 -0700

    updating ./roboshop-ansible

--- launch-instance-script/create-ec2.sh
+++ launch-instance-script/create-ec2.sh
@@ -23,7 +23,7 @@ create_ec2() {
 }
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-AMI_ID="ami-0eccf89e870877654"
+AMI_ID="ami-0bb6af715826253bf"
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 if [ "$COMPONENT" == "all" ]; then
