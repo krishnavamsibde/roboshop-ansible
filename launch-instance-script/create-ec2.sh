c8698f830ff38eacf5ef2c2941099fa71831a3a8
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-12 00:34:54 -0700

    updating ./roboshop-ansible

--- launch-instance-script/create-ec2.sh
+++ launch-instance-script/create-ec2.sh
@@ -6,13 +6,17 @@ SG_NAME="allow-all-to-public"
 IAM_INSTANCE_PROFILE="Arn=arn:aws:iam::794750663080:instance-profile/role-for-secret-manager-for-roboshop-components"
 #############################
 
-
+if [ -z "$ENV" ]; then
+  ENV=""
+else
+  ENV="-$ENV"
+fi
 COMPONENT=all
 create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
       --instance-type t2.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}]" \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
       --iam-instance-profile="${IAM_INSTANCE_PROFILE}" \
