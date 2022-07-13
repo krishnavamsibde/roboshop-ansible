6205de77911b6d171169278aff1e65acf619a4aa
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-12 19:40:18 -0700

    updating ./roboshop-ansible

--- launch-instance-script/create-ec2.sh
+++ launch-instance-script/create-ec2.sh
@@ -6,7 +6,7 @@ SG_NAME="allow-all-to-public"
 IAM_INSTANCE_PROFILE="Arn=arn:aws:iam::794750663080:instance-profile/role-for-secret-manager-for-roboshop-components"
 #############################
 
-if [ -z "${1}" ]; then
+if [ -z "${1}" ] ; then
   ENV=""
 else
   ENV="-$1"
